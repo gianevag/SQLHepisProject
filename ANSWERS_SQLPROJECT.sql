21ef7315101e09742e9a3b566829c65ea708e7a0
gianevag <gianevag@hotmail.com>
2017-02-10 12:02:37 +0200

    Testing the changes in sql project

--- ANSWERS_SQLPROJECT.sql
+++ ANSWERS_SQLPROJECT.sql
@@ -1,24 +1,24 @@
 ﻿USE InformationCompany
 GO
 
-/*ΕΡΩΤΗΜΑ 1*/
-SELECT cl_fname,cl_sname,cl_afm 
+/*ΕΡΩΤΗΜΑ 1_1*/
+SELECT cl_fname,cl_sname,cl_afm
 FROM (Client JOIN Municipality ON cl_mun=mun_id) JOIN Prefecture ON pref_id=mun_pref
 WHERE pref_name='ΑΤΤΙΚΗΣ'
 
-/*ΕΡΩΤΗΜΑ 2*/
-SELECT cl_fname,cl_sname,cl_afm 
+/*ΕΡΩΤΗΜΑ 2_1*/
+SELECT cl_fname,cl_sname,cl_afm
 FROM (Client JOIN Municipality ON cl_mun=mun_id) JOIN Prefecture ON pref_id=mun_pref
 WHERE pref_name !='ΑΤΤΙΚΗΣ' OR mun_name='ΑΘΗΝΩΝ'
 
-/*ΕΡΩΤΗΜΑ 3*/
+/*ΕΡΩΤΗΜΑ 3_1*/
 SELECT emp_fname,emp_sname,emp_salary
 FROM Employee
 WHERE datepart(YY,emp_datetime)>=05 AND datepart(MM,emp_datetime)>=01 AND datepart(DD,emp_datetime)>=01 AND emp_isContractor != 0
 
 /*ΕΡΩΤΗΜΑ 4*/
 SELECT pro_name,pro_detail
-FROM Project JOIN Client ON pro_client=cl_id 
+FROM Project JOIN Client ON pro_client=cl_id
 WHERE cl_name='STAR'
 
 /*ΕΡΩΤΗΜΑ 5*/
@@ -70,7 +70,7 @@ WHERE phase_description='ΠΑΡΑΔΟΣΗ' AND ProPhase_datetime BETWEEN '201610
 
 /*ΕΡΩΤΗΜΑ 10*/
 SELECT TOP(5) cl_name, SUM(pro_cost) AS cost
-FROM Client JOIN Project ON pro_client=cl_id 
+FROM Client JOIN Project ON pro_client=cl_id
 GROUP BY cl_name
 ORDER BY cost DESC
 
@@ -98,17 +98,17 @@ ORDER BY ProPhase_datetime DESC)V JOIN Tag_Project ON TagPro_pro_id = pro_id JOI
 and phase_description='ΠΑΡΑΔΟΣΗ'
 
 /*ΕΡΩΤΗΜΑ 14*/
-SELECT emp_fname, emp_sname, emp_email, emp_afm, emp_amka, emp_isContractor, emp_salary, emp_datetime, emp_zip 
+SELECT emp_fname, emp_sname, emp_email, emp_afm, emp_amka, emp_isContractor, emp_salary, emp_datetime, emp_zip
 FROM Employee JOIN Employee_Project ON EmpPro_emp_id=emp_id JOIN Project on EmpPro__pro_id=pro_id
 JOIN Tag_Project ON TagPro_pro_id = pro_id JOIN Tag ON tag_id=TagPro_tag_id
-WHERE (tag_name like '%WEB%') AND emp_isContractor = 1 
+WHERE (tag_name like '%WEB%') AND emp_isContractor = 1
 
 INTERSECT
 
-SELECT emp_fname, emp_sname, emp_email, emp_afm, emp_amka, emp_isContractor, emp_salary, emp_datetime, emp_zip 
+SELECT emp_fname, emp_sname, emp_email, emp_afm, emp_amka, emp_isContractor, emp_salary, emp_datetime, emp_zip
 FROM Employee JOIN Employee_Project ON EmpPro_emp_id=emp_id JOIN Project on EmpPro__pro_id=pro_id
 JOIN Tag_Project ON TagPro_pro_id = pro_id JOIN Tag ON tag_id=TagPro_tag_id
-WHERE (tag_name like '%PHP%') AND emp_isContractor = 1 
+WHERE (tag_name like '%PHP%') AND emp_isContractor = 1
 
 /*ΕΡΩΤΗΜΑ 15*/
 SELECT tag_name FROM (SELECT tag_name, COUNT(pro_name) AS number_of_project
@@ -120,7 +120,7 @@ WHERE V.number_of_project>=10
 SELECT B.emp_sname,B.emp_fname
 FROM (SELECT V.emp_sname,V.emp_fname,COUNT(V.cat_name) AS COUNT_category
 FROM (SELECT distinct emp_sname,emp_fname,cat_name
-FROM Employee JOIN Employee_Project ON emp_id=EmpPro_emp_id JOIN Project ON pro_id=EmpPro__pro_id 
+FROM Employee JOIN Employee_Project ON emp_id=EmpPro_emp_id JOIN Project ON pro_id=EmpPro__pro_id
 JOIN Category_Project on CatPro_pro_id=pro_id join Category on cat_id=CatPro_cat_id)V
 GROUP BY V.emp_sname, V.emp_fname)B
 WHERE B.COUNT_category = (SELECT COUNT(cat_name) FROM Category)
@@ -137,18 +137,18 @@ WHERE cl_fname='ΔΗΜΗΤΡΙΟΣ'
 /*ΕΡΩΤΗΜΑ 18*/
 SELECT emp_sname, emp_fname
 FROM Project join  Employee_Project on pro_id=EmpPro__pro_id join Employee on emp_id=EmpPro_emp_id
-join Project_Phase on pro_id=ProPhase_pro_id join Category_Project on CatPro_pro_id=pro_id 
+join Project_Phase on pro_id=ProPhase_pro_id join Category_Project on CatPro_pro_id=pro_id
 join Category on cat_id=CatPro_cat_id
 WHERE ProPhase_datetime BETWEEN '20160101' AND '20160630' AND ProPhase_phase_id=4 and emp_isContractor=0
-and cat_name not in 
-(SELECT cat_name 
-FROM  Project join Project_Phase on pro_id=ProPhase_pro_id join Category_Project on CatPro_pro_id=pro_id 
+and cat_name not in
+(SELECT cat_name
+FROM  Project join Project_Phase on pro_id=ProPhase_pro_id join Category_Project on CatPro_pro_id=pro_id
 join Category on cat_id=CatPro_cat_id
 WHERE ProPhase_datetime BETWEEN '20161001' AND '20161030' AND ProPhase_phase_id=2)
 
 /*ΕΡΩΤΗΜΑ 19*/
 alter procedure ContractorMoney (@date nvarchar(20))
-as 
+as
 begin
 
 	declare @amount float
@@ -162,37 +162,37 @@ select emp_id from Employee where emp_isContractor = 0
 set @amount = 0
 OPEN db_cursor
 FETCH NEXT FROM db_cursor INTO @contr
-WHILE @@FETCH_STATUS = 0   
-BEGIN   
-	
+WHILE @@FETCH_STATUS = 0
+BEGIN
+
 	set @amount = (select sum(pro_cost)from Project join  [dbo].[Employee_Project] on pro_id=EmpPro__pro_id join [dbo].[Project_Phase] on [ProPhase_pro_id]=pro_id
 					where EmpPro_emp_id=@contr and [ProPhase_phase_id]=4 and
 					datepart(MM,[ProPhase_datetime])=datepart(MM,convert(date,@date,5)) and
 					datepart(YY,[ProPhase_datetime])=datepart(YY,convert(date,@date,5)))
-	
-	set @count_contr = (select COUNT(emp_id) 
-					 from Employee join Employee_Project on emp_id=EmpPro_emp_id join Project on pro_id=EmpPro__pro_id 
+
+	set @count_contr = (select COUNT(emp_id)
+					 from Employee join Employee_Project on emp_id=EmpPro_emp_id join Project on pro_id=EmpPro__pro_id
 					 join Project_Phase on ProPhase_pro_id=pro_id
-					 where emp_isContractor = 0 and ProPhase_phase_id=4 
-					 and EmpPro_emp_id=@contr and 
+					 where emp_isContractor = 0 and ProPhase_phase_id=4
+					 and EmpPro_emp_id=@contr and
 					 datepart(MM,[ProPhase_datetime])=datepart(MM,convert(date,@date,5)) and
 					 datepart(YY,[ProPhase_datetime])=datepart(YY,convert(date,@date,5))
 					 group by EmpPro_emp_id)
 
-	set @contr_salary = @amount*0.20/@count_contr 
+	set @contr_salary = @amount*0.20/@count_contr
 
 	set @pro_number = (select count(pro_cost)from Project join  [dbo].[Employee_Project] on pro_id=EmpPro__pro_id join [dbo].[Project_Phase] on [ProPhase_pro_id]=pro_id
 					where EmpPro_emp_id=@contr and [ProPhase_phase_id]=4 and
 					datepart(MM,[ProPhase_datetime])=datepart(MM,convert(date,@date,5)) and
 					datepart(YY,[ProPhase_datetime])=datepart(YY,convert(date,@date,5)))
-	
+
 					IF (@pro_number >= 4)
-						
+
 					set	@contr_salary = @contr_salary + 200
-					
+
 
 					IF (@contr_salary<1200)
-						
+
 					set	@contr_salary = @contr_salary - @contr_salary*0.20
 
 					ELSE
@@ -205,7 +205,7 @@ BEGIN
 
 
 	FETCH NEXT FROM db_cursor INTO @contr
-END  
+END
 CLOSE db_cursor
 DEALLOCATE db_cursor
 END
@@ -224,16 +224,16 @@ AS
 	DECLARE @cancelDatetime DATETIME
 	DECLARE @cancel_project INT
 	DECLARE @phase int
-	
-	SELECT @cancelDatetime =i.ProPhase_datetime FROM inserted i;	
-	SELECT @phase = i.ProPhase_phase_id FROM inserted i;	
+
+	SELECT @cancelDatetime =i.ProPhase_datetime FROM inserted i;
+	SELECT @phase = i.ProPhase_phase_id FROM inserted i;
 	SELECT @cancel_project =i.ProPhase_pro_id FROM inserted i;
 
 	IF(@phase=6)
-		
+
 		INSERT INTO Cancel(cancel_datetime,cancel_project)
 		VALUES(@cancelDatetime,@cancel_project)
-		PRINT 'CANCEL trigger fired.'	
+		PRINT 'CANCEL trigger fired.'
 GO
 
 /*TEST TRIGGER*/
